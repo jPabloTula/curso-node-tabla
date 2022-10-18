7da7d2af78f8af3a0b4be6ad17bf00a6f7718907
Juan Pablo <juan@openix.com.ar>
2022-10-18 12:12:18 -0300

    Primer commit

---
+++ helpers/multiplicar.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const colors = require('colors');
+const crearArchivo = async (base = 5, listar = false, hasta = 5) => {
+    try {
+        
+        let salida = '';
+        let consola = '';
+
+        for (let index = 1; index <= hasta; index++) {
+            salida += `${base} x ${index} = ${base * index}\n`;
+            consola += `${base} ${'x'.green} ${index} ${'='.green} ${base * index}\n`;
+        }
+
+        if (listar) {
+            console.log('====================='.green);
+            console.log('  Tabla del:', colors.cyan(base));
+            console.log('====================='.green);
+            console.log(consola);
+        }
+
+        fs.writeFileSync(`./salida/tabla-${base}.txt`, salida);
+
+        return `Tabla-${base}.txt`;
+    } catch (error) {
+        throw error;
+    }   
+
+
+}
+
+module.exports = {
+    crearArchivo
+}
